9d6aa54b142dfe44e730ac56b938e7c5c4d0a7d2
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-27 21:35:32 +0530

    Exercise 4

---
+++ Javasctipt/Exercise 4/Exercise4.js
@@ -0,0 +1,17 @@
+// Program to make a keyless car
+// Name = Indrajeet Modnal; Date = 27th May 2023
+// SourceCode
+
+const prompt = require("prompt-sync")();
+
+let age = prompt("Enter your age:- ");
+
+if (age < 18) {
+    console.log("Sorry, you are too young to drive this car. Powering off");
+}
+else if (age == 18) {
+    console.log("Congratulations on your first year of driving. Enjoy the ride!");
+}
+else {
+    console.log("Powering On. Enjoy the ride!");
+}
