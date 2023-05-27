a59c9bc22944b4ca8253213fa056038e8964c0e1
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-27 09:37:41 +0530

    completed exercise 3

---
+++ Javasctipt/Exercise 3/Exercise3.js
@@ -0,0 +1,25 @@
+// Program to perform simple calculations
+// Name = Indrajeet Mondal; Date = 27th April 2023
+// SourceCode
+
+//To run in browser we don't need prompt-sync, prompt-sync is used to take input from the console/terminal; prompt is sufficient to take input from the browser as a pop-up
+const prompt = require("prompt-sync")();
+
+let a = prompt("Enter a number:- ");
+let b = prompt("Enter a number:- ");
+
+let sum = a + b;
+let product = a * b;
+let diff = a - b;
+
+console.log("The sum of " + a + " and " + b + " is " + sum + ".");
+console.log("The product of " + a + " and " + b + " is " + product + ".");
+console.log("The diff of " + a + " and " + b + " is " + diff + ".");
+
+if (b != 0) {
+    let quotient = a / b;
+    console.log("The quotient of " + a + " and " + b + " is " + quotient + ".");
+}
+else {
+    console.log("Division by zero not possible");
+}
