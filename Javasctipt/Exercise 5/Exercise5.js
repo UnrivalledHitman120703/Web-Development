153deebb437c4f3d5a40086900d23c33e30dad0f
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-28 06:55:38 +0530

    better version of if-else ladder

--- Javasctipt/Exercise 5/Exercise5.js
+++ Javasctipt/Exercise 5/Exercise5.js
@@ -31,10 +31,10 @@ let checkDriverAge2 = function () {
 // Bonus:
 function checkDriverAge(age) {
     if (Number(age) < 18) {
-        return "Sorry, you are too yound to drive this car. Powering off";
-    } else if (Number(age) > 18) {
-        return "Powering On. Enjoy the ride!";
-    } else if (Number(age) === 18) {
-        return "Congratulations on your first year of driving. Enjoy the ride!";
+        return("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) == 18) {
+        return("Powering On. Enjoy the ride!");
+    } else {
+        return("Congratulations on your first year of driving. Enjoy the ride!");
     }
 }
