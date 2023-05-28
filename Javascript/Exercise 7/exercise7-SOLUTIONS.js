bf09661c19cfec5ea048aea73a7a360a3b6c4042
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-28 09:49:22 +0530

    Exercise 7 and renaming the Exercise folder

---
+++ Javascript/Exercise 7/exercise7-SOLUTIONS.js
@@ -0,0 +1,33 @@
+// Trying to make a facebook clone 
+// Name = Indrajeet Mondal; Date = 28th May 2023
+// SourceCode
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+user = {
+	username: "andrei",
+	password: "supersecret"
+}
+
+// 2. Create an array which contains the object you have made above and name the arry "database"
+var database = [
+	{
+		username: "andrei",
+		password: "supersecret"
+	}
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
