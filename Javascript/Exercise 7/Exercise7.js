60d7f0b0847a6e1a36daf4004b0ce18e40292955
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-28 09:50:39 +0530

    some simple renaming

--- Javascript/Exercise 7/Exercise7.js
+++ Javascript/Exercise 7/Exercise7.js
@@ -3,13 +3,13 @@
 // SourceCode
 
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-user = {
+let user = {
 	username: "andrei",
 	password: "supersecret"
 }
 
 // 2. Create an array which contains the object you have made above and name the arry "database"
-var database = [
+let database = [
 	{
 		username: "andrei",
 		password: "supersecret"
@@ -17,7 +17,7 @@ var database = [
 ];
 
 // 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
-var newsfeed = [
+let newsfeed = [
 	{
 		username: "Bobby",
 		timeline: "So tired from all that learning!"
