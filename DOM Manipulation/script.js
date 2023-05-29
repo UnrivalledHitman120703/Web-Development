988ca633c0b1a6861772c3ac05f3f246db9c746f
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-29 23:22:54 +0530

    DOM event exercise

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -1,16 +1,46 @@
-let button = document.getElementById("enter");
-let input = document.getElementById("input");
-let ul = document.querySelector("ul");
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
 
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
-    let li = document.createElement("li");
+    var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
+
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode("Done!"));
+    li.appendChild(button);
+    button.onclick = underlineParent;
+    // CREATE AN ELEMENT
+    var button = document.createElement("button");
+    // APPEND TEXT TO BUTTON
+    button.appendChild(document.createTextNode("Delete!"));
+    // APPEND BUTTON TO LIST
+    li.appendChild(button);
+
+    button.onclick = removeParent;
+    //THIS FUNCTION WILL DO THIS FOR EVERY BUTTON CREATED.
+
+}
+
+// ul.onclick = function(event){
+// 	var target = event.target;
+// 	target.classList.toggle("done");
+// }
+
+function underlineParent(event) {
+    event.target.parentNode.classList.toggle("done");
+}
+
+
+function removeParent(evt) {
+    evt.target.parentNode.remove();
 }
 
 function addListAfterClick() {
@@ -19,12 +49,12 @@ function addListAfterClick() {
     }
 }
 
-function addListAfterKeyPress(event){
-    if(inputLength() > 0 && event.key === 'Enter'){
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
         createListElement();
     }
 }
 
 button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeyPress);
+input.addEventListener("keypress", addListAfterKeypress);
