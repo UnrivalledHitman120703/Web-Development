8a22acd76dff8164e636a98036b0211ed01dc41c
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2023-05-30 10:02:25 +0530

    DOM Background Generator exercise

--- DOM Manipulation/Background Generator/script.js
+++ DOM Manipulation/Background Generator/script.js
@@ -1,15 +1,10 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+function setGradient(){
+	body.style.background = "linear-gradient(to right,"+color1.value + ", "+ color2.value+")";
 
 	css.textContent = body.style.background + ";";
 }
